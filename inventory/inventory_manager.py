36a63d7091c39e563812e6d50a381a8db337decd
ryalipadma <ryali.padma@gmail.com>
2025-03-28 10:05:15 +0100

    inventory_manager

---
+++ inventory/inventory_manager.py
@@ -0,0 +1,23 @@
+from inventory.product import Product
+
+class InventoryManager:
+    def __init__(self):
+        pass
+    
+    def add_product(self, product: Product):
+        pass
+    
+    def remove_product(self, product_name: str):
+        pass
+    
+    def update_quantity(self, product_id: int):
+        pass
+    
+    def get_product_info(self, product_name: str):
+        pass
+    
+    def get_total_inventory_value(self):
+        pass
+    
+    
+
