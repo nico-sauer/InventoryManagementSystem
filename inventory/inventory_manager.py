e63491e0421afba642992eeec024743c87bcf3f9
WimpieRatte <assebliefaanvaar@gmail.com>
2025-03-28 11:35:13 +0100

    InventoryManager docstrings and implementation

--- inventory/inventory_manager.py
+++ inventory/inventory_manager.py
@@ -2,22 +2,41 @@ from inventory.product import Product
 
 class InventoryManager:
     def __init__(self):
-        pass
+        self.products = {}  # key = id, value = Product
     
     def add_product(self, product: Product):
-        pass
+        """Adds a new or overrides an existing product with the given product"""
+        self.products[product.id] = product
     
     def remove_product(self, product_name: str):
-        pass
+        """Removes a product by name"""
+        # get the id by name:
+        id = next((id for id, name in self.products.items() if name == product_name), None)
+        self.products.pop(id, None)  # "None" caters for if key was not found: then do nothing.
     
-    def update_quantity(self, product_id: int):
-        pass
+    def update_quantity(self, product_id: int, new_quantity: int):
+        """Update quantity of specified product"""
+        self.products[product_id].quantity = new_quantity
+    
+    def get_product_id(self, product_name: str):
+        """Gets the first id of a product by the given name"""
+        return next((id for id, name in self.products.items() if name == product_name), None)
     
     def get_product_info(self, product_name: str):
-        pass
+        """Retrieve product information by name"""
+        # get the id by name:
+        id = next((id for id, name in self.products.items() if name == product_name), None)
+        if id == None:
+            return "Product not found"
+        else:
+            return self.products[id].get_product_info()
     
     def get_total_inventory_value(self):
-        pass
+        """Calculate the total value of the entire inventory"""
+        total = 0.0  # initialise a float
+        for prod in self.products.values():
+            total += prod.price * prod.quantity
+        return total
     
     
 
