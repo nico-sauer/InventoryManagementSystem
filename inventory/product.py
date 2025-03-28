5316094727eaba21a5dafc1b0edb8c39e7fc3461
nico-sauer <nico.sauer@dci-student.org>
2025-03-28 09:43:04 +0100

    product class skeleton

---
+++ inventory/product.py
@@ -0,0 +1,16 @@
+class Product:
+    
+    def __init__(self):
+        
+        self.name = name 
+        self.price = price
+        self.quantity = quantity
+    
+    def update_quantity():
+        pass
+    
+    def update_price():
+        pass
+    
+    def get_product_info():
+        pass
