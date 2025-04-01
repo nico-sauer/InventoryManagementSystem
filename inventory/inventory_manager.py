4d7cef3ed629473411fafb728105504c3b5bde55
WimpieRatte <assebliefaanvaar@gmail.com>
2025-04-01 14:17:22 +0200

    fixed productid key

--- inventory/inventory_manager.py
+++ inventory/inventory_manager.py
@@ -29,19 +29,19 @@ class InventoryManager:
         """Removes a product by name"""
         # get the id by name:
         #id = next((id for id, name in self.products.items() if name == product_name), None)
-        self.products.pop(product_id, None)  # "None" caters for if key was not found: then do nothing.
+        self.products.pop(int(product_id), None)  # "None" caters for if key was not found: then do nothing.
     
     def update_quantity(self, product_id: int, new_quantity: int):
         """Update quantity of specified product"""
-        self.products[product_id].update_quantity(new_quantity)
+        self.products[int(product_id)].update_quantity(new_quantity)
         
     def update_price(self, product_id: int, new_price: float):
         """Update price of specified product"""
-        self.products[product_id].update_price(new_price)
+        self.products[int(product_id)].update_price(new_price)
     
     def update_cost_price(self, product_id: int, new_cost_price: float):
         """Update cost price of specified product"""
-        self.products[product_id].update_cost_price(new_cost_price)
+        self.products[int(product_id)].update_cost_price(new_cost_price)
     
     def get_product_id(self, product_name: str):
         """Gets the first id of a product by the given name"""
@@ -58,9 +58,9 @@ class InventoryManager:
         #     return "Product not found"
         # else:
         try:
-            return self.products[product_id].get_product_info()
-        except:
-            return "Product not found"
+            return self.products[int(product_id)].get_product_info()
+        except Exception as e:
+            return f"Product not found. Error: {e}"
         
     
     def get_total_inventory_value(self):
